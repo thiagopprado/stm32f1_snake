9cb2fc9b9ee90da217699c41ae4954a81a5ef073
Thiago Prado <thiagopereiraprado@gmail.com>
2023-01-05 15:34:37 -0300

    [XMAS_LEDS] 100us timer

--- applications/xmas_leds/Src/main.c
+++ applications/xmas_leds/Src/main.c
@@ -2,10 +2,46 @@
 #include <stdbool.h>
 
 #include "rcc.h"
+#include "timer.h"
 
+/** Definitions --------------------------------------------------- */
+
+/** Types --------------------------------------------------------- */
+
+/** Variables ----------------------------------------------------- */
+static volatile uint32_t timer_counter = 0;
+
+/** Prototypes ---------------------------------------------------- */
+static void timer_callback(void);
+static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout);
+
+/** Internal functions -------------------------------------------- */
+/**
+ * @brief Timer callback.
+ * 
+ * Executed every 100us.
+ */
+static void timer_callback(void) {
+    timer_counter++;
+}
+
+static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout) {
+    volatile uint32_t time_diff = timer_counter - timeshot;
+
+    if (time_diff >= timeout) {
+        return true;
+    }
+
+    return false;
+}
+
+/** Public functions ---------------------------------------------- */
 int main(void) {
     rcc_clock_init();
-    
-    while(true) {
+
+    timer_setup(TIMER_1, 71, 99);
+    timer_attach_callback(TIMER_1, timer_callback);
+
+    while (true) {
     }
 }
