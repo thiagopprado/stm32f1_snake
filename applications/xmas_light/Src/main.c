e19009b97905256211a66cb6d204cec2f50d9ba7
Thiago Prado <thiagopereiraprado@gmail.com>
2023-01-05 15:51:24 -0300

    [XMAS_LIGHT] LED strip update function

--- applications/xmas_light/Src/main.c
+++ applications/xmas_light/Src/main.c
@@ -20,6 +20,7 @@
  * 
  * @{
  */
+#define LED_UPDATE_TIME         50
 #define BUZZER_NOTE_TIME        1000
 /** @} */
 
@@ -49,11 +50,24 @@ static buzzer_note_t sheet_music[] = {
     BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST,
 };
 
+static uint32_t led_color[LED_WS2812_NR] = { 0 };
+
 /** Prototypes ---------------------------------------------------- */
+static void led_update(void);
+
 static void timer_callback(void);
 static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout);
 
 /** Internal functions -------------------------------------------- */
+/**
+ * @brief Updates the LED strip.
+ */
+static void led_update(void) {
+    memset(led_color, 0, sizeof(led_color));
+
+    led_ws2812_write(led_color, LED_WS2812_NR);
+}
+
 /**
  * @brief Timer callback.
  * 
@@ -77,17 +91,25 @@ static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout) {
 
 /** Public functions ---------------------------------------------- */
 int main(void) {
+    uint32_t led_update_timeshot = 0;
     uint32_t buzzer_timeshot = 0;
     uint32_t note_idx = 0;
 
     rcc_clock_init();
 
     buzzer_setup();
+    led_ws2812_setup();
 
     timer_setup(TIMER_1, 71, 99);
     timer_attach_callback(TIMER_1, timer_callback);
 
     while (true) {
+        if (timer_check_timeout(led_update_timeshot, LED_UPDATE_TIME) == true) {
+            led_update_timeshot = timer_counter;
+
+            led_update();
+        }
+
         if (timer_check_timeout(buzzer_timeshot, BUZZER_NOTE_TIME) == true) {
             buzzer_timeshot = timer_counter;
 
