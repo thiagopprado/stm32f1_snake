ce40c012b585bdea277e6122ff03680428e83aa5
Thiago Prado <thiagopereiraprado@gmail.com>
2023-01-05 15:41:27 -0300

    [XMAS_LEDS] Adds buzzer song

--- applications/xmas_leds/Src/main.c
+++ applications/xmas_leds/Src/main.c
@@ -1,15 +1,50 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+
+#include "stm32f1xx.h"
+#include "core_cm3.h"
 
 #include "rcc.h"
 #include "timer.h"
 
+#include "buzzer.h"
 /** Definitions --------------------------------------------------- */
+/**
+ * @brief Times.
+ * 
+ * Timer counter is incremented every 100us.
+ * 
+ * @{
+ */
+#define BUZZER_NOTE_TIME        1000
+/** @} */
 
 /** Types --------------------------------------------------------- */
 
 /** Variables ----------------------------------------------------- */
 static volatile uint32_t timer_counter = 0;
+
+static buzzer_note_t sheet_music[] = {
+    BUZZER_NOTE_E4, BUZZER_NOTE_ST, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_ST, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_ST, BUZZER_NOTE_G4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_C4, BUZZER_NOTE_ST, BUZZER_NOTE_D4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4,
+    BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4, BUZZER_NOTE_E4,
+    BUZZER_NOTE_F4, BUZZER_NOTE_ST, BUZZER_NOTE_F4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_F4, BUZZER_NOTE_F4, BUZZER_NOTE_F4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_F4, BUZZER_NOTE_ST, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_E4, BUZZER_NOTE_ST, BUZZER_NOTE_E4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_G4, BUZZER_NOTE_ST, BUZZER_NOTE_G4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_F4, BUZZER_NOTE_ST, BUZZER_NOTE_D4, BUZZER_NOTE_ST,
+    BUZZER_NOTE_C4, BUZZER_NOTE_C4, BUZZER_NOTE_C4, BUZZER_NOTE_C4,
+    BUZZER_NOTE_C4, BUZZER_NOTE_C4, BUZZER_NOTE_C4, BUZZER_NOTE_C4,
+    BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST,
+    BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST, BUZZER_NOTE_ST,
+};
 
 /** Prototypes ---------------------------------------------------- */
 static void timer_callback(void);
@@ -37,11 +72,26 @@ static bool timer_check_timeout(uint32_t timeshot, uint32_t timeout) {
 
 /** Public functions ---------------------------------------------- */
 int main(void) {
+    uint32_t buzzer_timeshot = 0;
+    uint32_t note_idx = 0;
+
     rcc_clock_init();
+
+    buzzer_setup();
 
     timer_setup(TIMER_1, 71, 99);
     timer_attach_callback(TIMER_1, timer_callback);
 
     while (true) {
+        if (timer_check_timeout(buzzer_timeshot, BUZZER_NOTE_TIME) == true) {
+            buzzer_timeshot = timer_counter;
+
+            buzzer_play_note(sheet_music[note_idx]);
+
+            note_idx++;
+            if (note_idx >= (sizeof(sheet_music) / sizeof(sheet_music[0]))) {
+                note_idx = 0;
+            }
+        }
     }
 }
